48e54c9ba437bf07a057c0b61151f8e0cd5c728b
Jack <bauer.jackw@gmail.com>
2019-01-23 01:11:24 +0000

    refactored appending to equation

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -39,78 +39,88 @@ public class MainActivity extends AppCompatActivity {
         binding.button0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                binding.editText.setText(binding.editText.getText() + "0");
+                appendToEquation("0");
             }
         });
 
         binding.button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                binding.editText.setText(binding.editText.getText() + "1");
+                appendToEquation("1");
+//                binding.equation.setText(binding.equation.getText() + "1");
             }
         });
 
         binding.button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                binding.editText.setText(binding.editText.getText() + "2");
+                appendToEquation("2");
+//                binding.equation.setText(binding.equation.getText() + "2");
             }
         });
 
         binding.button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                binding.editText.setText(binding.editText.getText() + "3");
+                appendToEquation("3");
+//                binding.equation.setText(binding.equation.getText() + "3");
             }
         });
 
         binding.button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                binding.editText.setText(binding.editText.getText() + "4");
+                appendToEquation("4");
+//                binding.equation.setText(binding.equation.getText() + "4");
             }
         });
 
         binding.button5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                binding.editText.setText(binding.editText.getText() + "5");
+                appendToEquation("5");
+//                binding.equation.setText(binding.equation.getText() + "5");
             }
         });
 
         binding.button6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                binding.editText.setText(binding.editText.getText() + "6");
+                appendToEquation("6");
+//                binding.equation.setText(binding.equation.getText() + "6");
             }
         });
 
         binding.button7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                binding.editText.setText(binding.editText.getText() + "7");
+                appendToEquation("7");
+//                binding.equation.setText(binding.equation.getText() + "7");
             }
         });
 
         binding.button8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                binding.editText.setText(binding.editText.getText() + "8");
+                appendToEquation("8");
+//                binding.equation.setText(binding.equation.getText() + "8");
             }
         });
 
         binding.button9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                binding.editText.setText(binding.editText.getText() + "9");
+                appendToEquation("9");
+//                binding.equation.setText(binding.equation.getText() + "9");
             }
         });
 
         binding.buttonDot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!binding.editText.getText().toString().contains(".")) {
-                    binding.editText.setText(binding.editText.getText() + ".");
+                if(!binding.equation.getText().toString().contains(".")) {
+                    appendToEquation(".");
+//                    binding.equation.setText(binding.equation.getText() + ".");
                 }
             }
         });
@@ -118,40 +128,52 @@ public class MainActivity extends AppCompatActivity {
         binding.buttonAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                computeCalculation();
-                CURRENT_ACTION = ADDITION;
-                binding.infoTextView.setText(decimalFormat.format(valueOne) + "+");
-                binding.editText.setText(null);
+                if(!"".equals(binding.equation.getText().toString())) {
+                    computeCalculation();
+                    CURRENT_ACTION = ADDITION;
+                    appendToEquation("+");
+//                    binding.equation.setText(decimalFormat.format(valueOne) + " + ");
+//                    binding.equation.setText(null);
+                }
             }
         });
 
         binding.buttonSubtract.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                computeCalculation();
-                CURRENT_ACTION = SUBTRACTION;
-                binding.infoTextView.setText(decimalFormat.format(valueOne) + "-");
-                binding.editText.setText(null);
+                if (!"".equals(binding.equation.getText().toString())) {
+                    computeCalculation();
+                    CURRENT_ACTION = SUBTRACTION;
+                    appendToEquation("-");
+//                binding.equation.setText(decimalFormat.format(valueOne) + " - ");
+//                binding.equation.setText(null);
+                }
             }
         });
 
         binding.buttonMultiply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                computeCalculation();
-                CURRENT_ACTION = MULTIPLICATION;
-                binding.infoTextView.setText(decimalFormat.format(valueOne) + "*");
-                binding.editText.setText(null);
+                if (!"".equals(binding.equation.getText().toString())) {
+                    computeCalculation();
+                    CURRENT_ACTION = MULTIPLICATION;
+                    appendToEquation(" * ");
+//                binding.equation.setText(decimalFormat.format(valueOne) + " * ");
+//                binding.equation.setText(null);
+                }
             }
         });
 
         binding.buttonDivide.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                computeCalculation();
-                CURRENT_ACTION = DIVISION;
-                binding.infoTextView.setText(decimalFormat.format(valueOne) + "/");
-                binding.editText.setText(null);
+                if (!"".equals(binding.equation.getText().toString())) {
+                    computeCalculation();
+                    CURRENT_ACTION = DIVISION;
+                    appendToEquation(" / ");
+//                binding.equation.setText(decimalFormat.format(valueOne) + " / ");
+//                binding.equation.setText(null);
+                }
             }
         });
 
@@ -159,8 +181,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 computeCalculation();
-                binding.infoTextView.setText(binding.infoTextView.getText().toString() + decimalFormat.format(valueTwo) + " = " + decimalFormat.format(valueOne));
-                valueOne = Double.NaN;
+//                binding.resultTextView.setText(binding.resultTextView.getText().toString() + decimalFormat.format(valueTwo) + " = " + decimalFormat.format(valueOne));
+                updateResult(valueOne);
+                //valueOne = Double.NaN;
                 CURRENT_ACTION = NONE;
             }
         });
@@ -168,17 +191,44 @@ public class MainActivity extends AppCompatActivity {
         binding.buttonClear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                binding.infoTextView.setText(null);
-                binding.editText.setText(null);
+                binding.resultTextView.setText(null);
+                binding.equation.setText(null);
+            }
+        });
+
+        binding.buttonClearEntry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.equation.setText(null);
             }
         });
 
     }
 
+    private void parseEquation() {
+
+    }
+
+    private void computeEquation() {
+
+    }
+
+    private void appendToEquation(String value) {
+        binding.equation.setText(binding.equation.getText() + value);
+    }
+
+    private void updateResult(double value) {
+        binding.resultTextView.setText(String.valueOf(value));
+    }
+
     private void computeCalculation() {
-        if(!Double.isNaN(valueOne)) {
-            valueTwo = Double.parseDouble(binding.editText.getText().toString());
-            binding.editText.setText(null);
+        if(Double.isNaN(valueOne)) {
+            try {
+                valueOne = Double.parseDouble(binding.equation.getText().toString());
+            } catch(Exception e) {}
+        } else {
+            valueTwo = Double.parseDouble(binding.equation.getText().toString());
+            binding.equation.setText(null);
 
             if(CURRENT_ACTION == ADDITION) {
                 valueOne += valueTwo;
@@ -186,14 +236,8 @@ public class MainActivity extends AppCompatActivity {
                 valueOne -= valueTwo;
             } else if(CURRENT_ACTION == MULTIPLICATION) {
                 valueOne *= valueTwo;
-        } else if(CURRENT_ACTION == DIVISION) {
+            } else if(CURRENT_ACTION == DIVISION) {
                 valueOne /= valueTwo;
-            }
-        } else {
-            try {
-                valueOne = Double.parseDouble(binding.editText.getText().toString());
-            } catch(Exception e){
-
             }
         }
     }
