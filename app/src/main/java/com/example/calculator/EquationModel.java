8396f9c56ac6b484a278e1e2f4489b4c3d2162fe
Jack <bauer.jackw@gmail.com>
2019-01-24 19:23:23 +0000

    no longer allows leading zeroes

--- app/src/main/java/com/example/calculator/EquationModel.java
+++ app/src/main/java/com/example/calculator/EquationModel.java
@@ -25,8 +25,8 @@ public class EquationModel {
 
     public void updateFullEquation() {
         String equation = "";
-        if(!this.valueOne.isEmpty()) {
-            if(this.hasDecimalOne) {
+        if (!this.valueOne.isEmpty()) {
+            if (this.hasDecimalOne) {
                 equation += this.valueOne;
             } else if (Double.valueOf(this.valueOne) % 1 == 0 && activeValue) {
                 equation += String.valueOf(Math.round(Double.valueOf(this.valueOne)));
@@ -36,7 +36,7 @@ public class EquationModel {
 
             if (this.operator != '\u0000') {  // '\u0000' is the default char value
                 equation += this.operator;
-                if(this.hasDecimalTwo) {
+                if (this.hasDecimalTwo) {
                     equation += this.valueTwo;
                 } else if (!this.valueTwo.isEmpty()) {
                     if (Double.valueOf(this.valueTwo) % 1 == 0) {
@@ -54,37 +54,45 @@ public class EquationModel {
         String digitString = String.valueOf(digit);
         String valueString = "";
 
-        if(operator == '\u0000') {
-            if(equaled) {
+        if (operator == '\u0000') {
+            if (equaled) {
                 this.valueOne = String.valueOf(digit);
                 equaled = false;
-            } else if(!this.valueOne.isEmpty()) {
-                    valueString = this.valueOne;
+            } else if (!this.valueOne.isEmpty()) {
+                valueString = this.valueOne;
             }
             this.valueOne = valueString + digitString;
+            if(Double.valueOf(this.valueOne) == 0 && !this.hasDecimalOne) {
+                this.valueOne = "0";
+            }
         } else {
             equaled = false;
-            if(!this.valueTwo.isEmpty()) {
-                    valueString = this.valueTwo;
+            if (!this.valueTwo.isEmpty()) {
+                valueString = this.valueTwo;
             }
             this.valueTwo = valueString + digitString;
+            if(Double.valueOf(this.valueTwo) == 0 && !this.hasDecimalTwo) {
+                this.valueTwo = "0";
+            }
         }
 
         updateFullEquation();
     }
 
     public void addDecimalToValue() {
-        if(!activeValue && !this.hasDecimalOne) {
-            if(this.valueOne.isEmpty()) {
+        if (!activeValue && !this.hasDecimalOne) {
+            if (equaled) {
+                this.valueOne = "0.";
+            } else if (this.valueOne.isEmpty()) {
                 this.valueOne += "0.";
             } else if (Double.valueOf(this.valueOne) % 1 == 0) {
                 this.valueOne = cleanValue(this.valueOne) + ".";
             }
             this.hasDecimalOne = true;
-        } else if (!this.hasDecimalTwo){
-            if(this.valueTwo.isEmpty()) {
+        } else if (!this.hasDecimalTwo) {
+            if (this.valueTwo.isEmpty()) {
                 this.valueTwo += "0.";
-            } else if (Double.valueOf(this.valueTwo) % 1 == 0){
+            } else if (Double.valueOf(this.valueTwo) % 1 == 0) {
                 this.valueTwo += ".";
             }
             this.hasDecimalTwo = true;
@@ -104,7 +112,7 @@ public class EquationModel {
     }
 
     public void solveEquation() {
-        if(!this.valueTwo.isEmpty()) {
+        if (!this.valueTwo.isEmpty()) {
             calculateEquation();
             clearEquation();
             this.valueOne = cleanValue(String.valueOf(this.solution));
@@ -114,7 +122,7 @@ public class EquationModel {
     }
 
     public void calculateEquation() {
-        if(!this.valueOne.isEmpty() && !this.valueTwo.isEmpty()) {
+        if (!this.valueOne.isEmpty() && !this.valueTwo.isEmpty()) {
             Double valueOne = Double.valueOf(this.valueOne);
             Double valueTwo = Double.valueOf(this.valueTwo);
             Double solution = valueOne;
@@ -150,7 +158,7 @@ public class EquationModel {
     }
 
     public void clearValue() {
-        if(this.operator == '\u0000') {
+        if (this.operator == '\u0000') {
             this.valueOne = "";
         } else {
             this.valueTwo = "";
@@ -165,14 +173,18 @@ public class EquationModel {
     }
 
     private String cleanValue(String value) {
-        if(value.charAt(value.length() - 1) == '.') {
+        if (value.charAt(value.length() - 1) == '.') {
             return value.substring(0, value.length() - 1);
         }
 
-        if(Double.valueOf(value) % 1 == 0) {
+        if (Double.valueOf(value) % 1 == 0) {
             return String.valueOf(Math.round(Double.valueOf(value)));
         }
 
         return value;
+    }
+
+    private String removeLeadingZeroes(String value) {
+        return value.replaceFirst("^0+(?!$)", "");
     }
 }
